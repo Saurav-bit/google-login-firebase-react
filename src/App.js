0b1b7ac01a66cf96e5854d5d77169aaffe519484
_Saurav_ <sauravsharma266@gmail.com>
2021-12-04 15:17:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-function App() {
+import { useState } from 'react';
+import Logout from './Logout';
+
+function App(props) {
+
+  // console.log(props.handlelogin)
+  // console.log(props.handlelogout)
+  
+
+  const [showlogoutButton, setShowlogoutButton] = useState(props.handlelogout);
+  const [showloginButton, setShowloginButton] = useState(props.handlelogin);
+//   const onSignoutSuccess = () => {
+//     alert("You have been logged out successfully");
+//     console.clear();
+//     setShowloginButton(true);
+//     setShowlogoutButton(false);
+// };
+
+const setlogin=(val)=>{
+ props.login(val)
+
+}
+const setlogout=(val)=>{
+  props.logout(val)
+
+}
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +44,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <Logout login={showloginButton} logout={showlogoutButton} handlelogout={setlogout} handlelogin={setlogin}/>
+      {/* <Logout /> */}
+
+
+
+      {/* <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Sign Out"
+                    onLogoutSuccess={onSignoutSuccess}
+                >
+                  </GoogleLogout> */}
     </div>
   );
 }
